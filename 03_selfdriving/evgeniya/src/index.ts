ccae9447cbbd58e705ba5ee6599e5fe7285e26ea
Evgeniya <evgeniya@codelane.ch>
2020-01-30 17:35:13 +0100

    evgeniyas first dummy solution (w/o writing to file)

---
+++ 03_selfdriving/evgeniya/src/index.ts
@@ -0,0 +1,59 @@
+import { readFile, Ride, Simulation, Vehicle, getScore, getDistance } from "./utils"
+import fs from 'fs'
+import _ from "lodash"
+
+async function main (filename: string) {
+  const input = await readFile('../../input/' + filename)
+
+  const sim: Simulation | undefined = input.sim
+  const rides = _.sortBy(input.rides, ['earliestStart']) //removed by length
+  const ridesDict: {[key: string]: Ride} = rides.reduce((map: {[key: string]: Ride}, obj) => ( map[obj.id] = obj, map ), {})
+  // console.log(ridesDict)
+  // console.log(rides)
+  const vehicles: Vehicle[] = []
+  let totalScore = 0
+
+  if (sim && rides) {
+    for (let i = 0; i < sim.vehicles; i++) {
+      // vehicles[`${i + 1}`] = new Vehicle(i + 1)
+      vehicles.push(new Vehicle(i))
+    }
+    while (rides.length) {
+      const r = rides.shift()
+      if (!r) break
+      for (const v of vehicles) {
+        const lastRide = v.rides.length ? ridesDict[v.rides[v.rides.length - 1]] : null
+        const potentialScore = getScore(lastRide, r, sim.bonus, sim.steps)
+        if (potentialScore) {
+          // console.log('potScore', potentialScore)
+          v.rides.push(r.id)
+          r.started = lastRide ? lastRide.willFinish + getDistance(lastRide.endPos, r.startPos) : getDistance([0, 0], r.startPos)
+          r.willFinish = r.started + r.length
+          totalScore += potentialScore
+          break
+        }
+      }
+    }
+  }
+
+
+
+  // const output = fs.createWriteStream('../../output/' + filename.slice(0, -4) + '_evgeniya.out', {
+  //   flags: 'a' // 'a' for appending
+  // })
+  // writeTofile(output, input.length)
+
+  // console.log('Vehicles: ', vehicles)
+  console.log('Score: ', totalScore)
+}
+
+// main('a_example.in')
+// main('b_should_be_easy.in')
+// main('c_no_hurry.in')
+// main('d_metropolis.in')
+// main('e_high_bonus.in')
+
+const files = fs.readdirSync('../../input/');
+for (const file of files) {
+  main(file)
+}
---
+++ 03_selfdriving/evgeniya/src/utils.ts
@@ -0,0 +1,107 @@
+import fs from 'fs'
+import es from 'event-stream'
+import getStream from 'get-stream'
+import { serializable, list, primitive, deserialize } from 'serializr'
+import _ from 'lodash'
+
+export class Simulation {
+  @serializable rows: number = 0
+  @serializable columns: number = 0
+  @serializable vehicles: number = 0
+  @serializable rides: number = 0
+  @serializable bonus: number = 0
+  @serializable steps: number = 0
+}
+
+export class Ride {
+  @serializable id: number = 0
+  @serializable(list(primitive())) startPos: number[] = []
+  @serializable(list(primitive())) endPos: number[] = []
+  @serializable earliestStart: number = 0
+  @serializable latestFinish: number = 0
+  assigned: boolean = false
+  started: number = 0
+  willFinish: number = 0
+
+  get length () {
+    return Math.abs(this.endPos[0] - this.startPos[0]) + Math.abs(this.endPos[1] - this.startPos[1])
+  }
+
+  get latestStart () {
+    return this.latestFinish - this.length
+  }
+}
+
+export class Vehicle {
+  @serializable id: number = 1
+  @serializable(list(primitive())) currentPos: number[] = [0, 0]
+  // @serializable status: number = 0 // 0 - free, 1 - busy
+  // nextRide: number | undefined = undefined
+  // currentRide: number | undefined = undefined
+  rides: number[] = []
+
+  constructor (id: number) {
+    this.id = id
+  }
+}
+
+export function getDistance (pos1: number[], pos2: number[]) {
+  return Math.abs(pos2[0] - pos1[0]) + Math.abs(pos2[1] - pos2[1])
+}
+
+export function getScore (ride1: Ride | null, ride2: Ride, bonus = 0, steps: number) {
+  let score = 0
+  // when ride2 can start
+  let canStart = ride1 ? ride1.willFinish + getDistance(ride1.endPos, ride2.startPos) : getDistance([0, 0], ride2.startPos)
+  if (canStart > ride2.latestStart) {
+    return 0
+  } else {
+    if (canStart <= ride2.earliestStart) {
+      canStart = ride2.earliestStart
+      // console.log('got bonus of', bonus)
+      score += bonus
+    } 
+    if (canStart + ride2.length > steps) {
+      return 0
+    } else {
+      score += ride2.length
+    }
+  }
+
+  return score
+}
+
+export async function readFile (filename: string) {
+  let idx = 0
+  let sim: Simulation = new Simulation()
+  // const rides: {[key: string]: Ride } = {}
+  const rides: Ride[] = []
+  const data = fs.createReadStream(filename)
+                  .pipe(es.split())
+                  .pipe(es.mapSync((line: any) => {
+                    const arr = line.split(' ').map((char: any) => Number(char))
+                    if (idx == 0) {
+                      sim = deserialize(Simulation, {rows: arr[0], columns: arr[1], vehicles: arr[2], rides: arr[3], bonus: arr[4], steps: arr[5]})
+                    } else {
+                      if (arr.length > 1) {
+                        const ride = deserialize(Ride, {id: idx - 1, startPos: [arr[0], arr[1]], endPos: [arr[2], arr[3]], earliestStart: arr[4], latestFinish: arr[5]})
+                        // rides[ride.id] = ride
+                        rides.push(ride)
+                      }
+                    }
+                      idx += 1
+                  })
+                  .on('error', function(err) {
+                      console.log('Error while reading file.', err);
+                  })
+                  .on('end', function() {
+                      // console.log('Read entire file.')
+                  }))
+
+  await getStream(data)
+  return { sim, rides }
+}
+// export function writeTofile (output: fs.WriteStream, line: number | Slide) {
+//   if (line instanceof Slide) output.write(_.join(line.pictures, ' ') + '\n')
+//   else output.write(`${line}\n`)
+// }
