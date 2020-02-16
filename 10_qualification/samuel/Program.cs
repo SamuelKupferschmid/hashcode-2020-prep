692363fde4aa86eeb71b34b7ec63756ae13e7f1e
Samuel Kupferschmid <samuel.kupferschmid@gmail.com>
2020-02-16 21:44:22 +0100

    add empty solution for qualification

---
+++ 10_qualification/samuel/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using SolutionLibrary;
+
+namespace Challenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var runner = new Runner<Qualification>(Path.Combine("..","..","..", ".."));
+            runner.RunInteractive();
+        }
+
+        public class Qualification : Solution
+        {
+            public override (int score, IList<string> output) Solve(string[] input)
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
