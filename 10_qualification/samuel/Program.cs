f3ea38b96983b450b77bd8529f1c6d786d7a9071
Samuel Kupferschmid <samuel.kupferschmid@gmail.com>
2020-02-20 16:43:46 +0100

    prepare qualification solution

--- 10_qualification/samuel/Program.cs
+++ 10_qualification/samuel/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.IO.Compression;
 using SolutionLibrary;
 
 namespace Challenge
@@ -15,10 +16,40 @@ namespace Challenge
 
         public class Qualification : Solution
         {
+            public readonly Configuration Config;
+
+            public Qualification()
+            {
+                Config = new Configuration
+                {
+
+                };
+            }
+
             public override (int score, IList<string> output) Solve(string[] input)
             {
-                throw new NotImplementedException();
+                // read input
+                var lineIndex = 0;
+
+                var ints = input[lineIndex++].SplitInts();
+                var rowCount = ints[0];
+                var colCount = ints[1];
+                var droneCount = ints[2];
+                var stepsCount = ints[3];
+                var maxLoad = ints[4];
+
+
+                var score = 0;
+                var resultLines = new List<string>();
+
+
+                return (score, resultLines);
             }
+        }
+
+        public class Configuration
+        {
+
         }
     }
 }
