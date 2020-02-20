cc32f6cf8c3af0cb04099999d723f7ab380a70a3
Samuel Kupferschmid <samuel.kupferschmid@gmail.com>
2020-02-20 20:17:00 +0100

    first version

--- 10_qualification/samuel/Program.cs
+++ 10_qualification/samuel/Program.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.IO;
 using System.IO.Compression;
+using System.Linq;
 using SolutionLibrary;
+using SolutionLibrary.Collections;
 
 namespace Challenge
 {
@@ -10,15 +12,15 @@ namespace Challenge
     {
         static void Main(string[] args)
         {
-            var runner = new Runner<Qualification>(Path.Combine("..","..","..", ".."));
+            var runner = new Runner<BookScanning>(Path.Combine("..","..","..", ".."));
             runner.RunInteractive();
         }
 
-        public class Qualification : Solution
+        public class BookScanning : Solution
         {
             public readonly Configuration Config;
 
-            public Qualification()
+            public BookScanning()
             {
                 Config = new Configuration
                 {
@@ -32,19 +34,130 @@ namespace Challenge
                 var lineIndex = 0;
 
                 var ints = input[lineIndex++].SplitInts();
-                var rowCount = ints[0];
-                var colCount = ints[1];
-                var droneCount = ints[2];
-                var stepsCount = ints[3];
-                var maxLoad = ints[4];
+                var bookCount = ints[0];
+                var libCount = ints[1];
+                var days = ints[2];
 
+                //var libBooks = new short[libCount, bookCount];
+                //return (0, new List<string>());
+
+                var books = input[lineIndex++].SplitInts().Select((val, index) => new Book { Index = index, Score = val}).ToList();
+
+                var unscannedLibBooks = new HashSet<int>[libCount];
+                var libSignupDays = new int[libCount];
+                var libRentLimit = new int[libCount];
+
+                
+
+                var libraries = new List<Library>();
+
+                for (int i = 0; i < libCount; i++)
+                {
+                    ints = input[lineIndex++].SplitInts();
+
+                    var libBooks = input[lineIndex++].SplitInts().Select(index => books[index]);
+                    var booksQueue = new PriorityQueue<Book>(libBooks, (b1, b2) => b1.Score - b2.Score);
+
+                    var lib = new Library
+                    {
+                        Id = i,
+                        SignupDays = ints[1],
+                        RentLimit = ints[2],
+                        Books = booksQueue,
+                    };
+
+
+                    libraries.Add(lib);
+                }
+
+                PriorityQueue<Library> signupQueue = new PriorityQueue<Library>(libraries, (library1, library2) => library1.SignupCost - library2.SignupCost );
+
+                var signedLibs = new List<Library>();
+                var scannedBooks = new HashSet<Book>();
+
+                var lastSignupCompletedAt = 0;
 
                 var score = 0;
+
+                for (int day = 0; day < days; day++)
+                {
+                    // signup libraries
+                    if (lastSignupCompletedAt <= day && signupQueue.Any())
+                    {
+                        ValidateQueue(signupQueue);
+                        var next = signupQueue.Dequeue();
+
+                        lastSignupCompletedAt = day + next.SignupDays;
+                        next.SignupCompleteAt = lastSignupCompletedAt;
+
+                        signedLibs.Add(next);
+
+                        
+                    }
+
+
+                    //ship books
+                    foreach (var lib in signedLibs.Where(l => l.SignupCompleteAt < day))
+                    {
+                        var remaining = lib.RentLimit;
+                        while (remaining-- > 0 && lib.Books.Any())
+                        {
+                            var book = lib.Books.Dequeue();
+                            
+                            if (!scannedBooks.Contains(book))
+                            {
+                                scannedBooks.Add(book);
+                                lib.Scanned.Add(book);
+                                score += book.Score;
+                            }
+                        }
+                    }
+
+                }
+
+                
                 var resultLines = new List<string>();
 
+                signedLibs = signedLibs.Where(l => l.SignupCompleteAt <= days && l.Scanned.Any()).ToList();
+
+                resultLines.Add(signedLibs.Count.ToString());
+
+                foreach (var signedLib in signedLibs)
+                {
+                    resultLines.Add(string.Join(' ', signedLib.Id, signedLib.Scanned.Count));
+                    resultLines.Add(string.Join(' ', signedLib.Scanned.Select(b => b.Index)));
+                }
 
                 return (score, resultLines);
             }
+
+            public void ValidateQueue(PriorityQueue<Library> queue)
+            {
+                // recalculate
+            }
+        }
+
+        public class Library
+        {
+            public int Id { get; set; }
+            public int SignupDays { get; set; }
+            public int SignupCost { get; set; }
+
+            public int SignupCompleteAt { get; set; }
+
+            public PriorityQueue<Book> Books { get; set; }
+
+            public List<Book> Scanned { get; set; } = new List<Book>();
+
+            public int BooksValue { get; set; }
+            public int RentLimit { get; set; }
+        }
+
+        public class Book
+        {
+            public int Index { get; set; }
+
+            public int Score { get; set; }
         }
 
         public class Configuration
